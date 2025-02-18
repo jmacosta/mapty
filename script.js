6580a5492e0f0be6c95206f92b48b38500d6570e
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2025-02-18 21:52:42 +0100

    Add Workout classes for Running and Cycling with pace and speed calculations

--- script.js
+++ script.js
@@ -10,7 +10,41 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
-let map, mapEvent;
+class Workout {
+  date = new Date();
+  id = (Date.now() + '').slice(-10);
+  constructor(coords, distance, duration) {
+    this.coords = coords; // [lat,lgt]
+    this.distance = distance;
+    this.duration = duration;
+  }
+}
+class Running extends Workout {
+  constructor(coords, distance, duration, elevation) {
+    super(coords, distance, duration);
+    this.elevation = elevation;
+    this.pace = this.calcPace();
+  }
+  calcPace() {
+    //min/km
+    this.pace = this.duration / this.distance;
+    return this.pace;
+  }
+}
+class Cycling extends Workout {
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+    this.calcSpeed();
+  }
+  calcSpeed() {
+    //min/km
+    this.speed = this.distance / this.duration / 60;
+    return this.speed;
+  }
+}
+
+///////////////////////////////////////////////////
 
 class App {
   #map;
